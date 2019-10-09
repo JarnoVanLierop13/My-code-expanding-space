981cd3797c396f2ffd3ebc1f6851042ddfdb2df2
Jarno van Lierop <24877@ma-web.nl>
2019-10-09 12:13:16 +0200

    Add files via upload

---
+++ ShieldPowerup.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldPowerup : MonoBehaviour
+{
+
+    public float duration = 5f;
+    public GameObject pickupEffect;
+    public Shield shield;
+    public static bool isinvulnerable;
+
+    // destroy object out of screen
+    new GameObject camera;
+    public Transform thisObject;
+    private bool isActive;
+
+
+    private void Start()
+    {
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
+        shield = GameObject.FindGameObjectWithTag("Shield").GetComponent<Shield>();
+    }
+
+    private void Update()
+    {
+        float minRangex = camera.transform.position.x - 10.5f;
+
+        if (thisObject.transform.position.x <= minRangex && isActive != true)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            isActive = true;
+            shield.Activate(duration);
+            Instantiate(pickupEffect, transform.position, transform.rotation);
+            OnPickup();
+        }
+    }
+
+    void OnPickup()
+    {
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<SpriteRenderer>().enabled = false;
+        Destroy(gameObject, duration);
+        isActive = false;
+    }
+}
---
+++ SlowDown.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowDown : MonoBehaviour {
+
+    public float multiplier = 1.4f;
+    public float duration = 3f;
+    public float modifier = 1.5f;
+    public GameObject pickupEffect;
+
+    // destroy object out of screen
+    new GameObject camera;
+    public Transform thisObject;
+    private bool isActive;
+
+    private void Start()
+    {
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    private void Update()
+    {
+        float minRangex = camera.transform.position.x - 10.5f;
+
+        if (thisObject.transform.position.x <= minRangex && isActive != true)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            StartCoroutine( Pickup(other) );
+        }
+    }
+
+    IEnumerator Pickup(Collider2D Player)
+    {
+        isActive = true;
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+
+        MoveWorld.WorldSpeed = MoveWorld.WorldSpeed / modifier;
+
+        yield return new WaitForSeconds(duration);
+
+        MoveWorld.WorldSpeed = MoveWorld.WorldSpeed * modifier;
+        isActive = false;
+        Destroy(gameObject);
+
+
+    }
+}
